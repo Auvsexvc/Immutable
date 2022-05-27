01c572aa7249e57667164d48c45b91a83ac9f7c4
Auvsexvc <auvsexvc@gmail.com>
2022-05-27 12:56:01 +0200

    Dodaj pliki projektów.

---
+++ Immutable/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Immutable;
+
+Console.WriteLine("Hello, World!");
+var human = new Human.Builder()
+            .Height(183)
+            .Weigth(73)
+            .Money(200)
+            .Build();
+human = new Human.Builder()
+            .Height(182)
+            .Weigth(71)
+            .Money(20)
+            .Build();
