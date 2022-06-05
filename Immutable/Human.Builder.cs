41d8b31e1c0e12de0a9a45295b0dfff20e90e2fd
Auvsexvc <auvsexvc@gmail.com>
2022-06-05 21:56:44 +0200

    Cleaned

---
+++ Immutable/Human.Builder.cs
@@ -0,0 +1,32 @@
+﻿namespace Immutable
+{
+    public partial class Human
+    {
+        public class Builder
+        {
+            private int height;
+            private int weigth;
+            private int money;
+
+            public Builder Height(int height)
+            {
+                this.height = height;
+                return this;
+            }
+
+            public Builder Weigth(int weigth)
+            {
+                this.weigth = weigth;
+                return this;
+            }
+
+            public Builder Money(int money)
+            {
+                this.money = money;
+                return this;
+            }
+
+            public Human Build() => new Human(height, weigth, money);
+        }
+    }
+}
--- Immutable/Human.cs
+++ Immutable/Human.cs
@@ -1,46 +1,19 @@
 ﻿namespace Immutable
 {
-    public class Human
+    public partial class Human
     {
-        private readonly int _height;
-        private readonly int _weigth;
-        private readonly int _money;
-        public int Height { get => _height; }
-        public int Weigth { get => _weigth; }
-        public int Money { get => _money; }
+        private readonly int height;
+        private readonly int weigth;
+        private readonly int money;
+        public int Height => height;
+        public int Weigth => weigth;
+        public int Money => money;
 
         private Human(int height, int weigth, int money)
         {
-            _height = height;
-            _weigth = weigth;
-            _money = money;
-        }
-
-        public class Builder
-        {
-            private int _height;
-            private int _weigth;
-            private int _money;
-
-            public Builder Height(int height)
-            {
-                _height = height;
-                return this;
-            }
-
-            public Builder Weigth(int weigth)
-            {
-                _weigth = weigth;
-                return this;
-            }
-
-            public Builder Money(int money)
-            {
-                _money = money;
-                return this;
-            }
-
-            public Human Build() => new Human(_height, _weigth, _money);
+            this.height = height;
+            this.weigth = weigth;
+            this.money = money;
         }
     }
 }
